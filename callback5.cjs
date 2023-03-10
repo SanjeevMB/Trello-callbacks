d2a3f531243103cd85987101a4437ebe55f0c336
= <=>
2023-03-10 16:06:36 +0530

    callback5.cjs, test/testCallback5.cjs
    using the previously written functions without giving controll to the callback and getting result

--- callback5.cjs
+++ callback5.cjs
@@ -1,7 +1,71 @@
 /* 
-	Problem 5: Write a function that will use the previously written functions to get the following information. You do not need to pass control back to the code that called it.
+    Problem 5: Write a function that will use the previously written functions to get the following information. You do not need to pass control back to the code that called it.
 
     Get information from the Thanos boards
     Get all the lists for the Thanos board
     Get all cards for the Mind and Space lists simultaneously
 */
+
+function usingPreviouslyWrittenFunction(thanosId, thonasBoard, mindList, spaceList) {
+
+    const boardInformation = require('./callback1.cjs');
+
+    const allListBelongToBoardId = require('./callback2.cjs');
+
+    const allCardsBelongsToListId = require('./callback3.cjs');
+
+    setTimeout(() => {
+
+        boardInformation(thanosId, (error, data) => {
+            if (error) {
+
+                console.error(error);
+
+            } else {
+
+                console.log('Information returned successfully');
+                console.log(data);
+            }
+        });
+        allListBelongToBoardId(thonasBoard, (error, data) => {
+            if (error) {
+
+                console.error(error);
+
+            } else {
+
+                console.log('Information returned successfully');
+                console.log(data);
+            }
+        });
+        allCardsBelongsToListId(mindList, (error, data) => {
+            if (error) {
+
+                console.error(error);
+
+            } else {
+
+                console.log('Card of listId return successfully');
+                console.log(data);
+
+            }
+        });
+        allCardsBelongsToListId(spaceList, (error, data) => {
+            if (error) {
+
+                console.error(error);
+
+            } else {
+
+                console.log('Card of listId return successfully');
+                console.log(data);
+
+            }
+
+        });
+
+    }, 2 * 1000);
+
+}
+
+module.exports = usingPreviouslyWrittenFunction;
---
+++ test/testCallback5.cjs
@@ -0,0 +1,3 @@
+const usingPreviouslyWrittenFunction = require('../callback5.cjs');
+
+usingPreviouslyWrittenFunction("mcu453ed", "mcu453ed", "qwsa221", "jwkh245");
